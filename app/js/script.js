0f3a4f3d41157445f45c439bd07ae0d46e80992f
cotnals44 <cotnals44@icloud.com>
2022-04-23 12:35:39 +0700

    Complete the Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("Test No2")
+const bthHamburger = document.querySelector("#btnHamburger")
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+
+bthHamburger.addEventListener('click', ()=>{
+    // console.log('Test')
+    if(header.classList.contains('open')){
+        //Close Hamburger
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    }else{
+        //Open Hamburger
+        header.classList.add('open');
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
+
+    }
+})
