a785c17a384e323b89e164f449442f3972c81e10
cotnals <su.chae@icloud.com>
2022-04-27 11:22:18 +0700

    Complete Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,30 @@
 
 const bthHamburger = document.querySelector("#btnHamburger")
+const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
 
 
 bthHamburger.addEventListener('click', ()=>{
     // console.log('Test')
     if(header.classList.contains('open')){
         //Close Hamburger
+        body.classList.remove('nonScroll')
         header.classList.remove('open')
-        overlay.classList.remove('fade-in')
-        overlay.classList.add('fade-out')
+        fadeElems.forEach((element)=>{
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
     }else{
         //Open Hamburger
+        body.classList.add('nonScroll')
         header.classList.add('open');
-        overlay.classList.remove('fade-out')
-        overlay.classList.add('fade-in')
+        fadeElems.forEach((element)=>{
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+        }
 
     }
-})
+)
